70e0285309320697d1d01016598cde8bc541793d
neosoft <hardik.j@neosoftmail.com>
2024-07-29 18:17:15 +0530

    initial commit

--- compressed/js/main.js
+++ compressed/js/main.js
@@ -4,7 +4,9 @@ let inputSearch = $(".search-box input").first()
 let faArrowLeft = $(".fa-arrow-left")
 let files = $("#files")
 let sendButton = $(".fas.fa-paper-plane")
-sendButton.on('click',()=> window.alert('Pressed'))
+
+
+
 let inputMsgBox = $(".content .container .message-box .message-content input")
 inputSearch.on('focus',()=>{
     header.addClass('focus');
@@ -16,16 +18,42 @@ faArrowLeft.on('click',()=>{
     header.removeClass('focus')
     files.removeClass('active')
 })
+
+
+handleNewMessage = function(){
+    inputMsgBox.val('')
+    inputMsgBox.trigger('input')
+}
+
+sendButton.on('click',handleNewMessage)
+
+inputMsgBox.on('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent the default form submit action
+        handleNewMessage();
+    }
+});
+
 inputMsgBox.on('input',()=>{
     // console.log(inputMsgBox.val().trim())
     if (inputMsgBox.val().trim() !== '') {
-        $(".fas.fa-microphone").css('display','none'); 
-        $(".fas.fa-paper-plane").css('display','block'); 
+        $(".fas.fa-microphone").hide(100); 
+        $(".fas.fa-paper-plane").show(200); 
     } else {
-        $(".fas.fa-paper-plane").css('display','none'); 
-        $(".fas.fa-microphone").css('display','block'); 
+        $(".fas.fa-paper-plane").hide(100); 
+        $(".fas.fa-microphone").show(200); 
     }
 })
+
+
+
+
+
+
+
+
+
+
 
 
 })
