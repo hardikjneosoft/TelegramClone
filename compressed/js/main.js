ee47cb7711acfc97debdca274ef6a5cc777399cf
neosoft <hardik.j@neosoftmail.com>
2024-07-30 11:40:57 +0530

    commit

--- compressed/js/main.js
+++ compressed/js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 let header = $(".header:first")
 let inputSearch = $(".search-box input").first()
@@ -20,9 +21,35 @@ faArrowLeft.on('click',()=>{
 })
 
 
+function getTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes;
+
+    const formattedTime = hours + ':' + minutesStr + ' ' + ampm;
+    return formattedTime;
+}
+
+
+
 handleNewMessage = function(){
+    msg = inputMsgBox.val()
     inputMsgBox.val('')
     inputMsgBox.trigger('input')
+    chatContainer = $('.chat-container')
+    sendMsg = `<div class="chat-msg">
+                        <p>${msg}</p>
+                        <span class="time">${getTime()}</span>
+                    </div><div class="chat-msg sent">
+                        <p>${msg}</p>
+                        <span class="time">${getTime()}</span>
+                    </div>`
+    $(chatContainer).append(sendMsg)
 }
 
 sendButton.on('click',handleNewMessage)
